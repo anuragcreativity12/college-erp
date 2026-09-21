6e98c3c85348198004f18a690a60f8e270a884bd
anuragcreativity12 <officialanuragmishra11@gmail.com>
2026-09-21 23:53:25 +0530

    Add links to dashboard menu items

--- dashboard.php
+++ dashboard.php
@@ -7,16 +7,16 @@ if($_SESSION['role'] != 'student'){
 ?>
 <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
 <ul>
-    <li>Timetable</li>
-    <li>Attendance</li>
-    <li>Assignments</li>
-    <li>Materials</li>
-    <li>Exams & Results</li>
-    <li>Fees</li>
-    <li>Notices</li>
-    <li>Faculty</li>
-    <li>Helpdesk</li>
-    <li>Resume & Career</li>
-    <li>Placements</li>
+    <li><a href="timetable.php">Timetable</a></li>
+    <li><a href="attendance.php">Attendance</a></li>
+    <li><a href="assignments.php">Assignments</a></li>
+    <li><a href="materials.php">Materials</a></li>
+    <li><a href="exams.php">Exams & Results</a></li>
+    <li><a href="fees.php">Fees</a></li>
+    <li><a href="notices.php">Notices</a></li>
+    <li><a href="faculty.php">Faculty</a></li>
+    <li><a href="helpdesk.php">Helpdesk</a></li>
+    <li><a href="resume.php">Resume & Career</a></li>
+    <li><a href="placements.php">Placements</a></li>
 </ul>
 <a href="logout.php">Logout</a>
